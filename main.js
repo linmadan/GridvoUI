81e588ced69f832be8e7e472f29c3505ce07d5ac
linmadan <772181827@qq.com>
2016-06-15 13:46:43 +0800

    重构UI，集成pomelo客户端API

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
 require('babel-polyfill');
 var React = require('react');
 var ReactDOM = require('react-dom');
-var MainDashBoard = require("./ui/mainDashBoard.js");
+var SelectStationPanel = require("./lib/ui/selectStationPanel.js");
+require('pomelo-cocos2d-js');
 
-ReactDOM.render(
-    <MainDashBoard stationName="NWHEJZ"/>,
-    document.getElementById('content')
-);
+var pomelo = window.pomelo;
+pomelo.init({
+    host: "127.0.0.1",
+    port: "3010",
+    log: true
+}, function () {
+    pomelo.request("stationConGate.stationConHandler.queryConnector", {}, function (result) {
+        pomelo.disconnect();
+        if (result.code === 500) {
+            console.log("query station connector server fail");
+            return;
+        }
+        pomelo.init({
+            host: result.host,
+            port: result.port,
+            log: true
+        }, function () {
+            pomelo.request("stationConnector.employeeEntryHandler.entry", {
+                accountID: "WeChatAccount",
+                accountType: "WeChat"
+            }, function (result) {
+                if (result.code === 500) {
+                    console.log("station employee entry fail");
+                    return;
+                }
+                window.stationNames = result.account.dutyStations;
+                ReactDOM.render(
+                    <SelectStationPanel/>,
+                    document.getElementById('content')
+                );
+            });
+        });
+    });
+});
